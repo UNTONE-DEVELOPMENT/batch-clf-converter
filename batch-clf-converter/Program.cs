85cdf85f7c28f3bfa4cb9949a5ae43c53f460c9e
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-11-02 13:00:59 +0000

    add clf-utils and fake-unity

---
+++ batch-clf-converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace batch_clf_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
