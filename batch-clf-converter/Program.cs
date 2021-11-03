7354851dc19949ac88de216257a3a624740e2ee2
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-11-03 15:18:45 +0000

    make it work

--- batch-clf-converter/Program.cs
+++ batch-clf-converter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using clf;
+using System.IO;
 
 namespace batch_clf_converter
 {
@@ -7,7 +8,33 @@ namespace batch_clf_converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! - " + args[1]);
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: batch-clf-converter.exe <input> <output> [debug code " + args.Length + "]");
+                Environment.Exit(-2);
+            }
+            //Console.WriteLine("Hello World! - " + args[0]);
+            bool areClfs = false;
+            string from = args[0];
+            string to = args[1];
+            if (!Directory.Exists(from))
+            {
+                Console.WriteLine("Input directory does not exist. Please make sure this folder exists & has atleast one CLF file in it.");
+                Environment.Exit(-1);
+            }
+            if (!Directory.Exists(to))
+            {
+                Console.WriteLine("Output directory does not exist. I'll create it for you.\n");
+                Directory.CreateDirectory(to);
+            }
+            DirectoryInfo d = new DirectoryInfo(from);
+            foreach (var file in d.GetFiles("*.clf"))
+            {
+                Console.WriteLine("Converting " + file.Name + "... (" + from + "/" + file.Name + ")\n");
+                clfFile newFile = clfUtils.convertLegacyFromFile(from + "/"  + file.Name);
+                Console.WriteLine("Done! Saving to " + file.Name + "... (" + to + file.Name + ")\n");
+                File.WriteAllText(to + "/" + file.Name, clfUtils.saveClfFile(newFile));
+            }
         }
     }
 }
