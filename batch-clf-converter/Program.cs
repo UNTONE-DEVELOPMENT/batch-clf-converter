94c085b53caf53181d9cb3c0f51b2c05e115aee8
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-11-04 13:16:21 +0000

    add comments & remove unnessecary code

--- batch-clf-converter/Program.cs
+++ batch-clf-converter/Program.cs
@@ -10,30 +10,37 @@ namespace batch_clf_converter
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: batch-clf-converter.exe <input> <output> [debug code " + args.Length + "]");
+                // user hasn't passed in any arguments (or hasnt passed enough, or too many) - just tell them how to use it
+                Console.WriteLine("Usage: batch-clf-converter.exe <input> <output>");
                 Environment.Exit(-2);
             }
-            //Console.WriteLine("Hello World! - " + args[0]);
-            bool areClfs = false;
-            string from = args[0];
-            string to = args[1];
-            if (!Directory.Exists(from))
+
+            string input = args[0];
+            string output = args[1];
+            if (!Directory.Exists(input))
             {
+                // input directory does not exist
                 Console.WriteLine("Input directory does not exist. Please make sure this folder exists & has atleast one CLF file in it.");
                 Environment.Exit(-1);
             }
-            if (!Directory.Exists(to))
+            if (!Directory.Exists(output))
             {
+                // output directory does not exist, create it for them
                 Console.WriteLine("Output directory does not exist. I'll create it for you.\n");
-                Directory.CreateDirectory(to);
+                Directory.CreateDirectory(output);
             }
-            DirectoryInfo d = new DirectoryInfo(from);
+            DirectoryInfo d = new DirectoryInfo(input);
             foreach (var file in d.GetFiles("*.clf"))
             {
-                Console.WriteLine("Converting " + file.Name + "... (" + from + "/" + file.Name + ")\n");
-                clfFile newFile = clfUtils.convertLegacyFromFile(from + "/"  + file.Name);
-                Console.WriteLine("Done! Saving to " + file.Name + "... (" + to + file.Name + ")\n");
-                File.WriteAllText(to + "/" + file.Name, clfUtils.saveClfFile(newFile));
+                Console.WriteLine("Converting " + file.Name + "... (" + input + "/" + file.Name + ")\n");
+
+                clfFile newFile = clfUtils.convertLegacyFromFile(input + "/"  + file.Name);
+                // create file using clf-utils
+
+                Console.WriteLine("Done! Saving to " + file.Name + "... (" + output + file.Name + ")\n");
+
+                File.WriteAllText(output + "/" + file.Name, clfUtils.saveClfFile(newFile));
+                // save using clf-utils
             }
         }
     }
